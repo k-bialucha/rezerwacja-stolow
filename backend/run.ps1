210f2af997a9178b8746a3d771001150e72ed03c
Kamil Bialucha <kamil.bialucha@altimi.com>
2018-03-20 08:32:24 +0100

    Run script simplified

--- backend/run.ps1
+++ backend/run.ps1
@@ -1,22 +1,5 @@
 param($envname = "newenv", $password = "")
 "Parameters: $envname, $password"
 .\activate.ps1 -envname $envname
-[Environment]::SetEnvironmentVariable("MYSQL_PASSWORD", $password, "User")
-    if (-not ("Win32.NativeMethods" -as [Type]))
-    {
-        # import sendmessagetimeout from win32
-        Add-Type -Namespace Win32 -Name NativeMethods -MemberDefinition @"
-    [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-    public static extern IntPtr SendMessageTimeout(
-        IntPtr hWnd, uint Msg, UIntPtr wParam, string lParam,
-        uint fuFlags, uint uTimeout, out UIntPtr lpdwResult);
-"@
-    }
- 
-    $HWND_BROADCAST = [IntPtr] 0xffff;
-    $WM_SETTINGCHANGE = 0x1a;
-    $result = [UIntPtr]::Zero
- 
-    # notify all windows of environment block change
-    [Win32.Nativemethods]::SendMessageTimeout($HWND_BROADCAST, $WM_SETTINGCHANGE, [UIntPtr]::Zero, "Environment", 2, 5000, [ref] $result);
-python BillardApp\testsite\manage.py runserver
+$env:MYSQL_PASSWORD = $password
+python BillardApp\testsite\manage.py runserver 0.0.0.0:8000
