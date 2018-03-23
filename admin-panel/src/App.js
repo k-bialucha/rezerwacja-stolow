4183199a8db58f02866565992033820a30484efa
Kamil Bialucha <210347@student.pwr.edu.pl>
2018-03-23 23:53:38 +0100

    Header component mounted inside the App component

--- admin-panel/src/App.js
+++ admin-panel/src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import CssBaseline from 'material-ui/CssBaseline';
+
+import Header from './Header/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <CssBaseline />
+        <Header />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
