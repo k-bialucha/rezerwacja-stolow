3c0e1751c8808bb771d121c0d8cbb802f4751001
Kamil Bialucha <210347@student.pwr.edu.pl>
2018-03-24 00:42:11 +0100

    AppBody component created

---
+++ admin-panel/src/AppBody/AppBody.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './AppBody.css';
+
+const AppBody = () =>
+    <div className="AppBody">
+        <p>There will be App Body</p>
+    </div>
+;
+
+export default AppBody;
