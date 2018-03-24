91d0c25c09ea8ae921f87c61b406b7a195e31a2d
Kamil Bialucha <210347@student.pwr.edu.pl>
2018-03-25 00:28:00 +0100

    Single reservation object template updated

--- admin-panel/src/components/Reservations/Reservations.js
+++ admin-panel/src/components/Reservations/Reservations.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Reservations = props => {
     const reservations = props.reservations
         .map( (reservation, index) => 
-            <div>{index+1}. {JSON.stringify(reservation)}</div>
+            <div className="Reservation">
+                <p>{index+1}. ID: {reservation['ID_RES']}</p>
+                <p>Użytkownik: {reservation['ID_USER']}</p>
+                <p>Stół nr: {reservation['ID_TABLE']}</p>
+                <p>Data: {reservation['DATE']} {reservation['HOUR_FROM']}-{reservation['HOUR_TO']} </p>
+            </div>
         );
     return (
         <div>
