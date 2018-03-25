4baef2c89034461777a55bcbab49f199663a7811
Kamil Bialucha <210347@student.pwr.edu.pl>
2018-03-25 01:19:34 +0100

    ReservationItem component created

---
+++ admin-panel/src/components/Reservations/ReservationItem.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import ExpansionPanel, {
+    ExpansionPanelSummary,
+    ExpansionPanelDetails,
+} from 'material-ui/ExpansionPanel';
+import Typography from 'material-ui/Typography';
+import ExpandMoreIcon from 'material-ui-icons/ExpandMore';
+
+const ReservationItem = props =>
+    <ExpansionPanel>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography variant="headline">
+                Rezeracja nr {props.id}
+            </Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+            <Typography>
+                Użytkownik: {props.userId} / Stół nr: {props.tableId} / {props.date}  [{props.startHour}-{props.endHour}]
+            </Typography>
+        </ExpansionPanelDetails>
+    </ExpansionPanel>
+;
+
+export default ReservationItem;
