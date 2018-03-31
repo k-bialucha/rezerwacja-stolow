117565e365a2f794af33c26d3290ebff6621e7e2
Kamil Bialucha <210347@student.pwr.edu.pl>
2018-03-31 19:27:42 +0200

    Starting HoursActivity removed from CalenderActivity

--- App/Billards2/app/src/main/java/com/example/sara/billards/CalenderActivity.java
+++ App/Billards2/app/src/main/java/com/example/sara/billards/CalenderActivity.java
@@ -1,8 +1,5 @@
 package com.example.sara.billards;
 
-import android.content.Context;
-import android.content.Intent;
-import android.icu.util.Calendar;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,7 +8,6 @@ import android.widget.Toast;
 
 public class CalenderActivity extends AppCompatActivity {
 CalendarView calendar;
-    Context context;
     String date;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +18,8 @@ CalendarView calendar;
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                Toast.makeText(getBaseContext(),"selected date:"+year+"/"+month+"/"+dayOfMonth,Toast.LENGTH_LONG).show();
-
-                 date= year+"/"+month+"/"+dayOfMonth; //format of date->date_id
-
-                Toast.makeText(getBaseContext(),"get date:"+date,Toast.LENGTH_LONG).show();
-                context = getApplicationContext();
-                Intent intent = new Intent(context, HoursActivity.class);
-                startActivity(intent);
+                date= year+"/"+month+"/"+dayOfMonth; //format of date->date_id
+                Toast.makeText(getBaseContext(),"Selected date:"+date, Toast.LENGTH_LONG).show();
             }
         });
     }
