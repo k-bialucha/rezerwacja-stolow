e814ece1c51e0073c1c4becdd1aa620930d69a6d
Kamil Bialucha <210347@student.pwr.edu.pl>
2018-04-01 10:43:03 +0200

    CalenderActivity updated to return date as a activity result

--- App/Billards2/app/src/main/java/com/example/sara/billards/CalenderActivity.java
+++ App/Billards2/app/src/main/java/com/example/sara/billards/CalenderActivity.java
@@ -1,5 +1,6 @@
 package com.example.sara.billards;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,8 +8,9 @@ import android.widget.CalendarView;
 import android.widget.Toast;
 
 public class CalenderActivity extends AppCompatActivity {
-CalendarView calendar;
+    CalendarView calendar;
     String date;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +22,10 @@ CalendarView calendar;
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
                 date= year+"/"+month+"/"+dayOfMonth; //format of date->date_id
                 Toast.makeText(getBaseContext(),"Selected date:"+date, Toast.LENGTH_LONG).show();
+                Bundle bundle = new Bundle();
+                bundle.putString("date", date);
+                setResult(RESULT_OK, new Intent().putExtras(bundle));
+                finish();
             }
         });
     }
