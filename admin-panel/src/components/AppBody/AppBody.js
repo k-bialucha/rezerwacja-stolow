bfdbb3d911e578dbe68cc958990693f7502c3363
Kamil Bialucha <210347@student.pwr.edu.pl>
2018-04-06 20:21:37 +0200

    NewReservationContainer component removed from the AppBody

--- admin-panel/src/components/AppBody/AppBody.js
+++ admin-panel/src/components/AppBody/AppBody.js
@@ -2,12 +2,10 @@ import React from 'react';
 
 import './AppBody.css';
 
-import NewReservationContainer from './../NewReservation/NewReservationContainer';
 import ReservationsContainer from '../Reservations/ReservationsContainer';
 
 const AppBody = () =>
     <div className="AppBody">
-        <NewReservationContainer />
         <ReservationsContainer />
     </div>
 ;
