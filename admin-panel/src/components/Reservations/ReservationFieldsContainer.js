1096ac4f5ed45c29e9208758ec91b53f4b8135dd
Kamil Bialucha <210347@student.pwr.edu.pl>
2018-04-08 22:44:06 +0200

    ReservationFields/Container initial components created

---
+++ admin-panel/src/components/Reservations/ReservationFieldsContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import ReservationFields from './ReservationFields';
+
+class ReservationFieldsContainer extends React.Component {
+    render() {
+        return <ReservationFields />;
+    }
+};
+
+export default ReservationFieldsContainer;
