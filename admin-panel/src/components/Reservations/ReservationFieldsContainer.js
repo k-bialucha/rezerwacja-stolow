4b9f2bef8f9f1891c308661b638b96415588bc10
Kamil Bialucha <210347@student.pwr.edu.pl>
2018-04-12 17:18:56 +0200

    Initial field values handled handled in the ReservationsFieldsContainer; fields and update function passed down

--- admin-panel/src/components/Reservations/ReservationFieldsContainer.js
+++ admin-panel/src/components/Reservations/ReservationFieldsContainer.js
@@ -3,8 +3,23 @@ import React from 'react';
 import ReservationFields from './ReservationFields';
 
 class ReservationFieldsContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userId: props.userId,
+            tableId: props.tableId,
+            date: props.date
+        }
+    }
+    updateField(event) {
+        const fieldName = event.target.name;
+        const value = event.target.value;
+        this.setState({
+            [fieldName]: value
+        });
+    }
     render() {
-        return <ReservationFields />;
+        return <ReservationFields {...this.state} updateField={this.updateField.bind(this)}/>;
     }
 };
 
