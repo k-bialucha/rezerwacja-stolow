c3978acdc5458398c904fb87fbbdea23d933955c
Kamil Bialucha <210347@student.pwr.edu.pl>
2018-04-12 17:22:54 +0200

    Proper fields (date, resId, tableId) shown in the ReservationsFields component

--- admin-panel/src/components/Reservations/ReservationFields.js
+++ admin-panel/src/components/Reservations/ReservationFields.js
@@ -1,9 +1,39 @@
 import React from 'react';
 
+import TextField from 'material-ui/TextField';
+import Grid from 'material-ui/Grid';
+
 const ReservationFields = props =>
-    <div>
-        ReservationFields - placeholder
-    </div>
-;
+    <Grid container spacing={24}>
+        <Grid item xs={6}>
+            <TextField
+                label="ID użytkownika"
+                name="userId"
+                value={props.userId}
+                onChange={event => props.updateField(event)}
+                fullWidth
+            />
+        </Grid>
+        <Grid item xs={6}>
+            <TextField
+                label="Data"
+                name="date"
+                value={props.date}
+                type="date"
+                onChange={event => props.updateField(event)}
+                fullWidth
+            />
+        </Grid>
+        <Grid item xs={6}>
+            <TextField
+                label="ID stołu"
+                name="tableId"
+                value={props.tableId}
+                onChange={event => props.updateField(event)}
+                fullWidth
+            />
+        </Grid>
+    </Grid>
+    ;
 
 export default ReservationFields;
