b078a5047d9fd5f764085ad61a392b0178e6ff99
Kamil Bialucha <210347@student.pwr.edu.pl>
2018-04-15 23:12:09 +0200

    Action buttons moved to the ExpansionPanelActions part in the Item component; withStyles HOC removed; component simplified

--- admin-panel/src/components/Reservations/Item.js
+++ admin-panel/src/components/Reservations/Item.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ExpansionPanel, {
     ExpansionPanelSummary,
     ExpansionPanelDetails,
+    ExpansionPanelActions
 } from 'material-ui/ExpansionPanel';
 import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
@@ -10,8 +11,6 @@ import Button from 'material-ui/Button';
 import DeleteIcon from 'material-ui-icons/Delete';
 import ExpandMoreIcon from 'material-ui-icons/ExpandMore';
 import SaveIcon from 'material-ui-icons/Save';
-
-import { withStyles } from 'material-ui/styles';
 
 import ItemFields from './ItemFields';
 
@@ -22,51 +21,31 @@ const Item = props =>
                 Rezerwacja nr {props.id}
             </Typography>
         </ExpansionPanelSummary>
-        <ExpansionPanelDetails 
-            className={props.classes.details}
-        >       
+        <ExpansionPanelDetails>       
             <ItemFields
                 userId={props.userId}
                 tableId={props.tableId}
                 date={props.date}
                 updateField={props.updateField}
             />
-            <Button 
-                variant="fab"
-                color="secondary"
-                className={props.classes.deleteButton}
-                mini
-                onClick={() => props.deleteItem()}    
-            >
-                <DeleteIcon />
-            </Button>
-            <Button 
-                variant="fab"
-                color="primary"
-                className={props.classes.updateButton}
-                mini
-                onClick={() => props.updateItem()}    
-            >
-                <SaveIcon />
-            </Button>
         </ExpansionPanelDetails>
+        <ExpansionPanelActions>
+            <Button 
+                color="secondary"
+                mini
+                onClick={props.deleteItem}    
+            >
+                <DeleteIcon /> Usuń
+            </Button>
+            <Button 
+                color="primary"
+                mini
+                onClick={props.updateItem}    
+            >
+                <SaveIcon /> Zapisz
+            </Button>
+        </ExpansionPanelActions>
     </ExpansionPanel>
 ;
 
-const classes = theme => ({
-    details: {
-        position: 'relative'
-    },
-    deleteButton: {
-        position: 'absolute',
-        bottom: '8px',
-        right: '8px'
-    },
-    updateButton: {
-        position: 'absolute',
-        bottom: '8px',
-        right: '60px'
-    }
-})
-
-export default withStyles(classes)(Item);
+export default Item;
