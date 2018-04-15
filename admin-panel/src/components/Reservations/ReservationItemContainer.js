811d4826b6e570a634ff7f1f1ab5b263fd360f8d
Kamil Bialucha <210347@student.pwr.edu.pl>
2018-04-15 21:53:12 +0200

    ReservationItemContainer updated to keep reservation item changes and post the updated values

--- admin-panel/src/components/Reservations/ReservationItemContainer.js
+++ admin-panel/src/components/Reservations/ReservationItemContainer.js
@@ -2,9 +2,39 @@ import React from 'react';
 
 import ReservationItem from './ReservationItem';
 
-class ReservationItemContainer extends React.Component {
+const FIELD_NAMES = {
+    userId: 'ID_USER',
+    tableId: 'ID_TABLE',
+    date: 'DATE'
+}
+
+class ReservationItemContainer extends React.PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = { };
+    }
+    updateField(event) {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value,
+            itemToUpdate: {
+                ...this.state.itemToUpdate,
+                [FIELD_NAMES[name]]: value
+            }
+        });
+    }
+    updateItem() {
+        this.props.updateItem(this.state.itemToUpdate)
+    }
     render() {
-        return <ReservationItem {...this.props} />
+        return (
+            <ReservationItem 
+                {...this.props}
+                {...this.state}
+                updateItem={this.updateItem.bind(this)} 
+                updateField={this.updateField.bind(this)} 
+            />
+        );
     }
 }
 
