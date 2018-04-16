12ab51be65c01368550ecadf36b17eafc29a8fa0
Kamil Bialucha <210347@student.pwr.edu.pl>
2018-04-17 01:32:39 +0200

    BottomNav component added to the App component; simple routing introduced

--- admin-panel/src/components/App.js
+++ admin-panel/src/components/App.js
@@ -5,17 +5,33 @@ import CssBaseline from 'material-ui/CssBaseline';
 
 import Header from './Header/Header';
 import AppBody from './AppBody/AppBody';
+import BottomNav from './BottomNav/BottomNav';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <CssBaseline />
-        <Header />
-        <AppBody />
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            route: 'reservations'
+        }
+    }
+    changeRoute(event, value) {
+        this.setState({ route: value })
+    }
+    render() {
+        return (
+            <div className="App">
+                <CssBaseline />
+                <Header />
+                <AppBody
+                    route={this.state.route}
+                />
+                <BottomNav
+                    route={this.state.route}
+                    changeRoute={this.changeRoute.bind(this)}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
