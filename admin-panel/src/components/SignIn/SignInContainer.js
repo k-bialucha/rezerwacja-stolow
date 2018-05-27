1b18e7023568f41f300e37b9d1f941dfc47a4a75
Kamil Bialucha <210347@student.pwr.edu.pl>
2018-05-27 23:37:29 +0200

    SignInContainer extended by withAuthContext HOC, authentication responsibility removed from the component

--- admin-panel/src/components/SignIn/SignInContainer.js
+++ admin-panel/src/components/SignIn/SignInContainer.js
@@ -1,50 +1,20 @@
 import React from 'react';
 
+import { withAuthContext } from '../../authContext';
+
 import IconButton from 'material-ui/IconButton';
 import AcountIcon from 'material-ui-icons/AccountCircle';
 
 import SignIn from './SignIn';
 
-const useLocalhost = true;
-const localhostUrl = 'http://localhost:8000/';
-const remoteUrl = 'http://ec2-18-217-215-212.us-east-2.compute.amazonaws.com:8000/';
-
-const apiUrl = useLocalhost ? localhostUrl : remoteUrl;
-const servicePath = 'login/';
-
-class SignInContainer extends React.Component {
+class SignInContainer extends React.PureComponent {
     constructor(props) {
         super(props);
-        const token = localStorage.getItem('apiToken');
         this.state = {
             isWindowOpen: false,
             username: '',
             password: '',
-            token
         }
-    }
-    setToken(token) {
-        localStorage.setItem('apiToken', token);
-        this.setState({ token })
-    }
-    signIn() {
-        const { username, password } = this.state;
-        const data = JSON.stringify(
-            { username, password }
-        );
-        fetch(apiUrl+servicePath, {
-            method: 'POST',
-            headers: {
-                'accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: data
-        })
-        .then( response => response.json() )
-        .then( json => {
-            this.setToken(json.token);
-            this.setState({ username: '', password: '' }) 
-        });
     }
     updateField(event) {
         const { name, value } = event.target;
@@ -58,6 +28,16 @@ class SignInContainer extends React.Component {
     handleClose = () => {
         this.setState({ isWindowOpen: false });
     }
+    handleSignIn() {
+        const { username, password } = this.state;
+        this.props.auth.signIn(username, password)
+            .then(() => {
+                this.setState({
+                    username: '',
+                    password: ''
+                });
+            })
+    }
     render() {
         return (
             <React.Fragment>
@@ -70,10 +50,10 @@ class SignInContainer extends React.Component {
                     updateField={this.updateField.bind(this)}
                     username={this.state.username}
                     password={this.state.password}
-                    signIn={this.signIn.bind(this)}
+                    signIn={this.handleSignIn.bind(this)}
                 />
             </React.Fragment>
         );
     }
 }
-export default SignInContainer;
+export default withAuthContext(SignInContainer);
