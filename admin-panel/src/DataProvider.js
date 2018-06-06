df2097e5d6d64022b3324c625130fec125e084f6
Kamil Bialucha <210347@student.pwr.edu.pl>
2018-06-06 23:06:39 +0200

    Configuration parameters added to the DataProvider class

--- admin-panel/src/DataProvider.js
+++ admin-panel/src/DataProvider.js
@@ -1,3 +1,17 @@
+const useLocalhost = true;
+const localhostUrl = 'http://localhost:8000/';
+const remoteUrl = 'http://ec2-18-217-215-212.us-east-2.compute.amazonaws.com:8000/';
+
+const apiUrl = useLocalhost ? localhostUrl : remoteUrl;
+
+const endpoints = {
+    reservations: 'testsite/api2/'
+}
+
+const composeUrl = resource => 
+    apiUrl+endpoints[resource]
+;
+
 class DataProvider {
     constructor(token) {
         this.token = token;
