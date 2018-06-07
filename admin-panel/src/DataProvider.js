83a9fa1edaed2bfffe9b84289d78b854b84bd9de
Kamil Bialucha <210347@student.pwr.edu.pl>
2018-06-07 21:33:16 +0200

    Service URL composer function updated

--- admin-panel/src/DataProvider.js
+++ admin-panel/src/DataProvider.js
@@ -1,15 +1,15 @@
 const useLocalhost = true;
-const localhostUrl = 'http://localhost:8000/';
-const remoteUrl = 'http://ec2-18-217-215-212.us-east-2.compute.amazonaws.com:8000/';
+const localhostUrl = 'http://localhost:8000';
+const remoteUrl = 'http://ec2-18-217-215-212.us-east-2.compute.amazonaws.com:8000';
 
 const apiUrl = useLocalhost ? localhostUrl : remoteUrl;
 
 const endpoints = {
-    reservations: 'testsite/api2/'
+    reservations: 'testsite/api2'
 }
 
-const composeUrl = resource => 
-    apiUrl+endpoints[resource]
+const composeUrl = (resource, key) => 
+    `${apiUrl}/${endpoints[resource]}/${key ? key+'/' : ''}`
 ;
 
 class DataProvider {
